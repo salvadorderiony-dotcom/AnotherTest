6390fe72e9b91fc16cfee1e9fe31473247b48e21
ЫФВЫФВЫВ <salvadorderiony@gmail.com>
2025-09-26 19:42:45 +0300

    Commited by ****

--- FFFFFFFFFF/src/main/java/NewClass.java
+++ FFFFFFFFFF/src/main/java/NewClass.java
@@ -2,5 +2,6 @@ public class NewClass {
     public static void main(String[] args) {
         System.out.println("BLA");
         System.out.println("ANOTHER BLA");
+        System.out.println("ANOTHER BLA");
     }
 }

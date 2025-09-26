fe74b93db0a8cd6d0de1cb9aa838a994abfa6be7
ЫФВЫФВЫВ <salvadorderiony@gmail.com>
2025-09-26 19:47:55 +0300

    Commited by ****

--- FFFFFFFFFF/src/main/java/NewClass2.java
+++ FFFFFFFFFF/src/main/java/NewClass2.java
@@ -1,5 +1,9 @@
 public class NewClass2 {
     public static void main(String[] args) {
         System.out.println("NOOOOO");
+        System.out.println("NOOOOO");
+        System.out.println("NOOOOO");
+        System.out.println("NOOOOO");
+        System.out.println("NOOOOO");
     }
 }
